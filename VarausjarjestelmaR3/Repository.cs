0458660e03916a44189498a279d85c21a78c9976
Ansa Allt <2306080@edu.karelia.fi>
2024-04-24 19:30:20 +0300

    Tulostusmetodi ja käyttöohje. Repository.cs

---
+++ VarausjarjestelmaR3/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace VarausjarjestelmaR3
+{
+    internal class Repository
+    {
+
+
+
+
+
+        public static void PrintVisual(FrameworkElement pageToPrint)             //Metodi, joka avaa printti-ikkunan 
+        {
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                printDialog.PrintVisual(pageToPrint, "Print Information");
+            }
+
+
+            //Koodi jolla luodaan printti elementistä:
+            //FrameworkElement pageToPrint = GetPageToPrint();
+            //Nimeä tulostettava elementti, esim window tässä tapauksessa, tähän
+
+
+            //Tämä lähettää sen PrintVisual-metodille, joka avaa tulostusikkunan.
+            //Repository.PrintVisual(pageToPrint);
+        }
+    }
+}
