91ef790c423751f4ddb95dfb3b33196dea7db131
Paula Timonen <2201746@edu.karelia.fi>
2024-05-01 21:57:00 +0300

    Pari muutosta luokkiin (Classes).

--- VarausjarjestelmaR3/Classes/Reservation.cs
+++ VarausjarjestelmaR3/Classes/Reservation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace VarausjarjestelmaR3.Classes
 {
@@ -16,9 +17,10 @@ namespace VarausjarjestelmaR3.Classes
         public String Lisatiedot { get; set; }
         public Customer Asiakas { get; set; }
         public Employee Tyontekija { get; set; }
+        public ObservableCollection<ReservationServices> VarauksenPalvelut { get; set; }
     }
 
-    internal class ReservationServices
+    public class ReservationServices
     {
         public int PalveluvarausID { get; set; }
         public Service Palvelu {  get; set; }
