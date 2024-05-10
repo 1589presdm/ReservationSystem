7ea98c2973036297da1cf413b36373c763eddadd
Ansa Allt <2306080@edu.karelia.fi>
2024-05-10 16:52:43 +0300

    Raportointi minimivaatimustasolle.

--- VarausjarjestelmaR3/Report.xaml.cs
+++ VarausjarjestelmaR3/Report.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,24 +16,24 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace VarausjarjestelmaR3
-    {
+{
     /// <summary>
     /// Interaction logic for Report.xaml
     /// </summary>
     public partial class Report : UserControl
-        {
+    {
         Repository repo;
 
 
-        public Report ()
+        public Report()
+        {
             {
-                {
                 InitializeComponent();
                 repo = new Repository();
-                }
-
             }
-        private void GetInfo_Click (object sender, RoutedEventArgs e)
+
+        }
+        private void GetInfo_Click(object sender, RoutedEventArgs e)
         //{
         //    DataTable dataTable = repo.GetInvoicesForPrint();
 
@@ -41,42 +42,43 @@ namespace VarausjarjestelmaR3
 
         //    dataGridInvoices.ItemsSource = dataTable.DefaultView;
         //}
-            {//untested, waiting for code fixes in customers.xaml
-            // Fetch data from the repository
+
+        {
+                                                                                                        // Fetch data from the repository
             DataTable dataTable = repo.GetInvoicesForPrint();
 
-            // Get the selected start and end dates
+                                                                                                        // Get the selected start and end dates
             DateTime startDate = StartDatePicker.SelectedDate ?? DateTime.MinValue;
             DateTime endDate = EndDatePicker.SelectedDate ?? DateTime.MaxValue;
 
-            // Filter the data based on the date range
+                                                                                                       // Filter the data based on the date range
             DataView dataView = new DataView(dataTable);
             dataView.RowFilter = $"varaus_alkaa >= '{startDate.ToString("yyyy-MM-dd")}' AND varaus_alkaa <= '{endDate.ToString("yyyy-MM-dd")}'";
 
             // Update the DataGrid with the filtered data
             dataGridInvoices.ItemsSource = dataView;
-            }
-
-        private void PrintButton_Click (object sender, RoutedEventArgs e)
-            {
-                
-
-                // Luodaan PrintDialog-olio
-                PrintDialog printDialog = new PrintDialog();
-
-                if (printDialog.ShowDialog() == true)
-                    {
-                    // Luo RenderTargetBitmap-olio ja piirrä DataGrid sen päälle
-                    RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap((int)dataGridInvoices.ActualWidth, (int)dataGridInvoices.ActualHeight, 96, 96, PixelFormats.Default);
-                    renderTargetBitmap.Render(dataGridInvoices);
-
-                    // Luo Image-olio ja aseta sille renderöity kuva
-                    Image image = new Image();
-                    image.Source = renderTargetBitmap;
-
-                    // Käynnistä tulostus
-                    printDialog.PrintVisual(image, "Tulosta DataGrid");
-                    }
-            }
         }
+
+
+        private void PrintButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap((int)ActualWidth, (int)ActualHeight, 96, 96, System.Windows.Media.PixelFormats.Pbgra32);
+            renderTargetBitmap.Render(this);
+
+            PngBitmapEncoder pngEncoder = new PngBitmapEncoder();                                            //Create screenshot as PNG
+            pngEncoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+            MemoryStream memoryStream = new MemoryStream();
+            pngEncoder.Save(memoryStream);
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);              //Save to desktop
+            string fileName = $"UI_Screenshot_{DateTime.Now:yyyyMMdd_HHmmss}.png";
+            string filePath = System.IO.Path.Combine(desktopPath, fileName);
+            File.WriteAllBytes(filePath, memoryStream.ToArray());
+
+            MessageBox.Show($"Kuva tallennettu työpöydälle nimellä: {fileName}", "Tallennus onnistui", MessageBoxButton.OK, MessageBoxImage.Information);  //Confirmation box
+        }
+        
     }
+}
